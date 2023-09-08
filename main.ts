3e6acde8012f2cd76e36bc779a972a85ce9b91c1
Hampus Toft <136370606+hato1883@users.noreply.github.com>
2023-09-08 11:19:21 +0200

    Start of Repeater program

--- main.ts
+++ main.ts
@@ -1,13 +1,27 @@
+enum RadioMessage {
+    message1 = 49434
+}
 input.onButtonPressed(Button.A, function () {
     if (DeviceID == 0) {
         DeviceID = NextID
         NextID += 1
         radio.sendValue("NextID", NextID)
+    } else {
+    	
     }
+    radio.sendValue("1111111", 0)
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "NextID") {
         NextID = value
+    }
+    if (parseFloat(name.substr(DeviceID - 1, 1)) == 1) {
+        radio.sendValue("1111111", value)
+        basic.showNumber(DeviceID)
+        basic.pause(500)
+        basic.showNumber(value)
+        basic.pause(500)
+        basic.clearScreen()
     }
 })
 function IdNotSet () {
@@ -17,13 +31,10 @@ function IdNotSet () {
 }
 let DeviceID = 0
 let NextID = 0
-radio.setGroup(123)
+radio.setGroup(137)
+radio.sendMessage(RadioMessage.message1)
 NextID = 1
 DeviceID = 0
-basic.forever(function () {
-    if (DeviceID == 0) {
-        IdNotSet()
-    } else {
-        basic.showNumber(DeviceID)
-    }
-})
+while (DeviceID == 0) {
+    IdNotSet()
+}
