99086cfa07b3970a6f27680de9952daff4211681
Hampus Toft <136370606+hato1883@users.noreply.github.com>
2023-09-10 20:50:19 +0200

    Changed Extenders to read a String message. Now allows for 32 extenders (2*16)

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
 enum RadioMessage {
     message1 = 49434
 }
+/**
+ * Set DeviceID to 0
+ */
+/**
+ * Set NextID to 1
+ */
+/**
+ * Set Radio Group
+ */
+/**
+ * then loop as long as device has not ben initialized with a ID
+ */
+/**
+ * debug - radio send a trash message
+ */
 // IF
 // DeviceID has no been set, then set DeviceID = NextID and then broadcast the increase of NextID
 // ELSE
@@ -11,21 +26,66 @@ input.onButtonPressed(Button.A, function () {
         NextID += 1
         radio.sendValue("NextID", NextID)
     } else {
-        index = 1
+        loopIndex = 1
         SendID = ""
-        while (index < DeviceID) {
-            SendID = "" + SendID + "1"
+        charcode = 0
+        while (loopIndex <= 16) {
+            if (loopIndex != DeviceID) {
+                charcode = charcode + 2 ** (loopIndex - 1)
+            }
+            loopIndex += 1
         }
-        SendID = "" + SendID + "0"
-        while (SendID.length < 7) {
-            SendID = "" + SendID + "1"
-        }
-        radio.sendValue(SendID, 0)
+        SendID = "" + String.fromCharCode(2 ** 16 - 1) + String.fromCharCode(charcode) + String.fromCharCode(255) + String.fromCharCode(35) + String.fromCharCode(1023)
+        radio.sendString(SendID)
     }
+})
+function toCharCode (char: string) {
+    toCharCodecharcode = 0
+    loopIndex = 15
+    while (char != String.fromCharCode(toCharCodecharcode)) {
+        if (char.compare(String.fromCharCode(2 ** loopIndex - 1 + toCharCodecharcode)) < 0) {
+            loopIndex += -1
+        } else {
+            toCharCodecharcode += 2 ** (loopIndex - 1)
+        }
+    }
+    return toCharCodecharcode
+}
+function shouldExtend (charcode: number) {
+    shouldExtendcharcode = charcode
+    loopIndex = 0
+    while (16 - loopIndex >= DeviceID) {
+        if (2 ** (16 - loopIndex) <= shouldExtendcharcode) {
+            shouldExtendcharcode = shouldExtendcharcode - 2 ** (16 - loopIndex)
+        }
+        loopIndex += 1
+    }
+    return shouldExtendcharcode / 2 ** (DeviceID - 1) >= 1
+}
+radio.onReceivedString(function (receivedString) {
+    if (DeviceID > 16) {
+        extenderSignal = toCharCode(receivedString.charAt(0))
+        if (shouldExtend(extenderSignal)) {
+            basic.showNumber(toCharCode(receivedString.charAt(3)))
+            extenderSignal = extenderSignal - 2 ** (DeviceID - 1)
+            radio.sendString("" + String.fromCharCode(extenderSignal) + receivedString.charAt(1) + receivedString.substr(2, receivedString.length - 1))
+        }
+    } else {
+        extenderSignal = toCharCode(receivedString.charAt(1))
+        if (shouldExtend(extenderSignal)) {
+            animateExtension()
+            extenderSignal = extenderSignal - 2 ** (DeviceID - 1)
+            radio.sendString("" + receivedString.charAt(0) + String.fromCharCode(extenderSignal) + receivedString.substr(2, receivedString.length - 1))
+        } else {
+            basic.showIcon(IconNames.No)
+            basic.clearScreen()
+        }
+    }
+    basic.showIcon(IconNames.Heart)
 })
 // IF
 // name == "NextID" then change our NextID to be equal to new.
-// ELSE 
+// ELSE
 // check if we receive a string of 1's and 0's.
 // For example 1001001 = value
 // One Exist in spot 7  /|\
@@ -34,31 +94,67 @@ input.onButtonPressed(Button.A, function () {
 radio.onReceivedValue(function (name, value) {
     if (name == "NextID") {
         NextID = value
-    } else if (parseFloat(name.substr(DeviceID - 1, 1)) == 1) {
-        leftID = name.substr(0, DeviceID - 1)
-        rightID = name.substr(DeviceID, name.length - DeviceID)
-        reciverID = "" + leftID + convertToText(0) + rightID
-        radio.sendValue(reciverID, value)
-    } else {
-    	
     }
 })
+function animateExtension () {
+    images.createImage(`
+        . # # # .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `).showImage(0)
+    basic.clearScreen()
+    images.createImage(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `).showImage(0)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        # . # . .
+        . . . . .
+        . . . . .
+        `).showImage(0)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . #
+        . . . . .
+        . . . . .
+        `).showImage(0)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `).showImage(0)
+    basic.clearScreen()
+    images.createImage(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . # # # .
+        `).showImage(0)
+    basic.clearScreen()
+}
 // Animate what the next available ID is
 function IdNotSet () {
     basic.showNumber(NextID)
     basic.clearScreen()
     basic.pause(1000)
 }
-// Set Radio Group
-// debug - radio send a trash message
-// Set NextID to 1
-// Set DeviceID to 0
-// then loop as long as device has not ben initialized with a ID
-let reciverID = ""
-let rightID = ""
-let leftID = ""
+let extenderSignal = 0
+let shouldExtendcharcode = 0
+let toCharCodecharcode = 0
+let charcode = 0
 let SendID = ""
-let index = 0
+let loopIndex = 0
 let DeviceID = 0
 let NextID = 0
 radio.setGroup(137)
@@ -68,3 +164,4 @@ DeviceID = 0
 while (DeviceID == 0) {
     IdNotSet()
 }
+basic.showIcon(IconNames.Heart)
